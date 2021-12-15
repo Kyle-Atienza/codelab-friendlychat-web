90d88b0b7aab2e020f2047d94401c35a7f98447c
Kyle-Atienza <atienzakylefrancis@gmail.com>
2021-12-15 13:46:33 +0800

    initial commit

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -6,9 +6,18 @@
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
  */
+//  import { initializeApp } from "firebase/app";
+
 const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  apiKey: "AIzaSyDtD1nfToTRoYTIJS8W53ygbOdtlnEUQu8",
+  authDomain: "friendlychat-d1649.firebaseapp.com",
+  projectId: "friendlychat-d1649",
+  storageBucket: "friendlychat-d1649.appspot.com",
+  messagingSenderId: "207186312185",
+  appId: "1:207186312185:web:cb7d4fbe664e8ffb711b28"
 };
+
+// const app = initializeApp(config);
 
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
